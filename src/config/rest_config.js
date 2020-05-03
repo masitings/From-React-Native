3cd1dee55cfb46025acbf3bf4730a2de1da9661f
Rafi Halilintar <masitingss@gmail.com>
2020-05-03 16:42:55 +0700

    Oke

---
+++ src/config/rest_config.js
@@ -0,0 +1,3 @@
+export const endpoint = 'http://power.vpnlabs.xyz/api/v1';
+export const user_id = 'd210378d-e435-4a8c-bd8b-fc079047eb37';
+export const camp_id = '06c4decc-6f8d-4a5b-933a-07eb10f29223';
---
+++ src/screens/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export class HomeScreen extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <Text>Home Screen</Text>
+        );
+    }
+
+}
